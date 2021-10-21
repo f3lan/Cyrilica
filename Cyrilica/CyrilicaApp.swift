6692bd94b525acad0de6b8f97f44fda66eddde2d
Felix Langenegger <felix.langenegger@fadendaten.ch>
2021-10-21 14:03:59 +0200

    Initial Commit

---
+++ Cyrilica/CyrilicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyrilicaApp.swift
+//  Cyrilica
+//
+//  Created by Felix on 21.10.21.
+//
+
+import SwiftUI
+
+@main
+struct CyrilicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
